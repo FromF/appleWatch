9123b18be6e203e517d02268bef91a90d0a6843d
FF <haruhito.fuji@gmail.com>
2015-01-24 17:39:52 +0900

    タップすると記事のタイトルだけを表示するが、READボタン押すと何故か２件目以降がうまく表示されないです・・・(bugってる？)

--- WatchKitTest WatchKit Extension/InterfaceController.m
+++ WatchKitTest WatchKit Extension/InterfaceController.m
@@ -12,7 +12,7 @@
 
 @interface InterfaceController()
 @property (weak, nonatomic) IBOutlet WKInterfaceTable *mytable;
-
+@property (strong , nonatomic) NSMutableArray *entries;
 @end
 
 
@@ -41,28 +41,43 @@
 }
 
 - (IBAction)readBtnAction {
-    NSString *url = @"http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://rss.dailynews.yahoo.co.jp/fc/rss.xml&num=10";
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
-    NSData *json_data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
-    NSError *error;
-    NSDictionary *json_parsed = [NSJSONSerialization JSONObjectWithData:json_data
-                                                            options:NSJSONReadingAllowFragments
-                                                            error:&error];
-
-    NSDictionary *responseData = [json_parsed objectForKey:@"responseData"];
-    NSDictionary *feedData = [responseData objectForKey:@"feed"];
-    NSArray *entries = [feedData objectForKey:@"entries"];
+    if ([self.entries count] == 0) {
+        NSString *url = @"http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://rss.dailynews.yahoo.co.jp/fc/rss.xml&num=10";
+        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+        NSData *json_data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+        NSError *error;
+        NSDictionary *json_parsed = [NSJSONSerialization JSONObjectWithData:json_data
+                                                                    options:NSJSONReadingAllowFragments
+                                                                      error:&error];
+        
+        NSDictionary *responseData = [json_parsed objectForKey:@"responseData"];
+        NSDictionary *feedData = [responseData objectForKey:@"feed"];
+        self.entries = [feedData objectForKey:@"entries"];
+    }
     
-    NSInteger count = [entries count];
+    NSInteger count = [self.entries count];
     
     [self.mytable setNumberOfRows:count withRowType:@"default"];
     
     for (int i = 0 ; i < count ; i++ ) {
-        NSDictionary *entrie = [entries objectAtIndex:i];
+        NSDictionary *entrie = [self.entries objectAtIndex:i];
         NSString *title = [entrie objectForKey:@"title"];
+        //NSLog(@"%@",title);
         tablecustom *row = [self.mytable rowControllerAtIndex:i];
         [row.label setText:title];
     }
+}
+
+- (void)table:(WKInterfaceTable *)table didSelectRowAtIndex:(NSInteger)rowIndex {
+    
+    NSInteger i = rowIndex;
+    [self.mytable setNumberOfRows:1 withRowType:@"default"];
+    NSDictionary *entrie = [self.entries objectAtIndex:i];
+    NSString *title = [entrie objectForKey:@"title"];
+    tablecustom *row = [self.mytable rowControllerAtIndex:0];
+    [row.label setText:title];
+
+    
 }
 
 @end
